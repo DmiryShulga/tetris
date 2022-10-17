08fcd8fde29ee47ed8677af99c07823722c9928d
DmiryShulga <115163240+DmiryShulga@users.noreply.github.com>
2022-10-17 23:31:22 +0300

    Добавлен поворот фигуры

--- forTest.js
+++ forTest.js
@@ -9,34 +9,32 @@ learConsole.onclick = function(){
     console.clear()
 }
 showPalyfild.onclick = function(){
-    game.rotatePiece()
-    console.log(game.activePiece.blocks)
+   console.log(game.playfied)
 }
 // КОНСОЛЬ
-
-console.log(Boolean(0))
 // Передвижение
 const left = document.querySelector('.left')
 const right = document.querySelector('.right')
 const down = document.querySelector('.down')
+const rotate = document.querySelector('.rotate')
 
 left.onclick = function(){
    game.movePieceLeft()
-//    console.clear()
-console.log(game.playfied)
 }
 
 right.onclick = function(){
     game.movePieceRight()
-    console.log(game.playfied)
 }
 down.onclick = function(){
     game.movePieceDown()
-    console.log(game.playfied)
 }
 show.onclick = function(){
     game.lockPiece()
     console.log(game.playfied)
+}
+rotate.onclick = function(){
+    game.rotatePiece()
+    console.log(game.activePiece.blocks)
 }
 // Передвижение 
 
--- scr/game.js
+++ scr/game.js
@@ -76,19 +76,29 @@ export default class Game {
             this.activePiece.y -= 1;
         }
     };
-
+//  поворот и проверка столкновения фигуры
     rotatePiece(){
-        this.activePiece.rotationsIndex = (this.activePiece.rotationsIndex + 1) % 4
+        this.activePiece.rotationsIndex = this.activePiece.rotationsIndex < 3 ? this.activePiece.rotationsIndex + 1 : 0
+        if(this.hasCollision()){
+           
+            this.activePiece.rotationsIndex = this.activePiece.rotationsIndex > 0 ? this.activePiece.rotationsIndex - 1 : 3
+            console.log(` Активный блок ${this.activePiece.rotations}`)
+        }
         return this.activePiece.blocks 
     }
 
     // Проверка на столкновения фигуры 
     hasCollision() {
         const { y: pieceY, x: pieceX, blocks } = this.activePiece
+        console.log(blocks)
         for (let y = 0; y < blocks.length; y++) {
             for (let x = 0; x < blocks[y].length; x++) {
+                // console.log("Количесто итераций в массиве" +number)
+                // console.log(`Выход за пределы поля низ = ${this.playfied[pieceY + y] === undefined} : Проверка блока ${Boolean(blocks[y][x])}`)
+                // console.log(`Выход за пределы поля бока ${ this.playfied[pieceY + y][pieceX + x] === undefined} :  Проверка блока ${Boolean(blocks[y][x])} `)
+                // console.log(`Столкновения с другой фигурой ${ Boolean(this.playfied[pieceY + y][pieceX + x]) } : Проверка блока ${Boolean(blocks[y][x])} `)
                 if (
-                    blocks[y][x] && (this.playfied[pieceY + y] === undefined || this.playfied[pieceY + y][pieceX + x] === undefined || this.playfied[pieceY + y][pieceX + x])) {
+                    blocks[y][x] && ((this.playfied[pieceY + y] === undefined || this.playfied[pieceY + y][pieceX + x] === undefined) )) {
                     return true
                 }
             }
@@ -98,6 +108,8 @@ export default class Game {
     // Отрисовка фигуры
     lockPiece() {
         const { y: pieceY, x: pieceX, blocks } = this.activePiece
+        console.log(pieceY)
+        console.log(blocks)
         for (let y = 0; y < blocks.length; y++) {
             for (let x = 0; x < blocks[y].length; x++) {
                 if (blocks[y][x]) {
