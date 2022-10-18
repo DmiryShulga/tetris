499c5ec311ddc6a72bca2065b4f0874cf2839a35
DmiryShulga <115163240+DmiryShulga@users.noreply.github.com>
2022-10-18 18:29:14 +0300

    Тестирования гетторов, сетторов на этапе пока все не сломал

--- forTest.js
+++ forTest.js
@@ -1,6 +1,8 @@
 const testGame = window.game
 const  [showPalyfild ,learConsole] = document.querySelectorAll('.btnsConsol')[0].children
-const [show, left, right, down, rotate] = document.querySelectorAll('.btnsDirections')[0].children
+const [left, right, down, show, rotate] = document.querySelectorAll('.btnsDirections')[0].children
+
+console.log(game.activePiece.blocks)
 
 learConsole.onclick = function(){
     console.clear()
@@ -18,20 +20,18 @@ right.onclick = function(){
     game.movePieceRight()
 }
 
-{ showPalyfild ,learConsole}
-
 down.onclick = function(){
     game.movePieceDown()
 }
 
 show.onclick = function(){
-    game.lockPiece()
-    console.log(game.playfied)
+    game.lockPiece
 }
 
 rotate.onclick = function(){
     game.rotatePiece()
     console.log(game.activePiece.blocks)
 }
-
-
+ 
+let a = new Array(20).fill([]) 
+console.log(a)
--- scr/game.js
+++ scr/game.js
@@ -2,131 +2,134 @@ export default class Game {
     score = 0;
     lines = 0;
     level = 0;
-    playfied = [
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]
-    ];
-    activePiece = {
-        x: 0,
-        y: 0,
-
-        get blocks() {
-
-            return this.rotations[this.rotationsIndex]
-        },
-        rotationsIndex: 0,
-        rotations: [
-            [
-                [0, 1, 0],
-                [1, 1, 1],
-                [0, 0, 0]
-            ],
-            [
-                [0, 1, 0],
-                [0, 1, 1],
-                [0, 1, 0]
-            ],
-            [
-                [0, 0, 0],
-                [1, 1, 1],
-                [0, 1, 0]
-            ],
-            [
-                [0, 1, 0],
-                [1, 1, 0],
-                [0, 1, 0]
-            ],
-        ]
-    };
-
-    movePieceLeft() {
-        this.activePiece.x -= 1
-
-        if (this.hasCollision()) {
-            this.activePiece.x += 1;
-
-        }
-    };
-
-    movePieceRight() {
-        this.activePiece.x += 1
-
-        if (this.hasCollision()) {
-            this.activePiece.x -= 1;
-        }
-
-
-    };
     
-    movePieceDown() {
-        this.activePiece.y += 1
+    playfied = {
+    get () {
 
-        if (this.hasCollision()) {
-            this.activePiece.y -= 1;
-        }
-    };
+        let array = []
 
-    //  поворот и проверка столкновения фигуры
-    rotatePiece() {
-        this.activePiece.rotationsIndex = this.activePiece.rotationsIndex < 3 ? this.activePiece.rotationsIndex + 1 : 0
-
-        if (this.hasCollision()) {
-            this.activePiece.rotationsIndex = this.activePiece.rotationsIndex > 0 ? this.activePiece.rotationsIndex - 1 : 3
-            console.log(` Активный блок ${this.activePiece.rotations}`)
+        for (let i = 0; i < 20; i++) {
+            array[i] = new Array(10).fill(0)
         }
 
-        return this.activePiece.blocks
-    }
-
-    // Проверка на столкновения фигуры 
-    hasCollision() {
+        return array
+    },
+    set lockPiece(value) {
         const { y: pieceY, x: pieceX, blocks } = this.activePiece
-
-        for (let y = 0; y < blocks.length; y++) {
-
-            for (let x = 0; x < blocks[y].length; x++) {
-
-                if (
-                    blocks[y][x] && ((this.playfied[pieceY + y] === undefined || this.playfied[pieceY + y][pieceX + x] === undefined))) {
-
-                    return true
-                }
-            }
-        }
-
-        return false
-    };
-
-    // Отрисовка фигуры
-    lockPiece() {
-        const { y: pieceY, x: pieceX, blocks } = this.activePiece
-
+        console.log(blocks)
+        this.playfied[0][0] = 54
         for (let y = 0; y < blocks.length; y++) {
 
             for (let x = 0; x < blocks[y].length; x++) {
 
                 if (blocks[y][x]) {
+                    console.log("Я работаю")
+
                     this.playfied[pieceY + y][pieceX + x] = blocks[y][x];
                 }
             }
         }
     }
+};
+
+
+activePiece = {
+    x: 0,
+    y: 0,
+    blocks: [
+        [0, 1, 0],
+        [1, 1, 1],
+        [0, 0, 0]
+    ]
+};
+
+movePieceLeft() {
+    this.activePiece.x -= 1
+
+    if (this.hasCollision()) {
+        this.activePiece.x += 1;
+
+    }
+};
+
+movePieceRight() {
+    this.activePiece.x += 1
+
+    if (this.hasCollision()) {
+        this.activePiece.x -= 1;
+    }
+
+
+};
+
+movePieceDown() {
+    this.activePiece.y += 1
+
+    if (this.hasCollision()) {
+        this.activePiece.y -= 1;
+    }
+};
+
+//  поворот и проверка столкновения фигуры
+rotatePiece() {
+    const blocks = this.activePiece.blocks
+    const lengthBlocks = blocks.length
+    const temp = [];
+
+    for (let i = 0; i < lengthBlocks; i++) {
+        temp[i] = new Array(lengthBlocks).fill(0)
+    }
+
+    for (let y = 0; y < lengthBlocks; y++) {
+
+        for (let x = 0; x < lengthBlocks; x++) {
+            temp[x][y] = blocks[lengthBlocks - 1 - y][x]
+        }
+
+    }
+
+    this.activePiece.blocks = temp
+
+    if (this.hasCollision) {
+        this.activePiece.blocks = blocks
+    }
+}
+
+// Проверка на столкновения фигуры 
+hasCollision() {
+    const { y: pieceY, x: pieceX, blocks } = this.activePiece
+
+    for (let y = 0; y < blocks.length; y++) {
+
+        for (let x = 0; x < blocks[y].length; x++) {
+
+            if (
+                blocks[y][x] && ((this.playfied[pieceY + y] === undefined || this.playfied[pieceY + y][pieceX + x] === undefined))) {
+
+                return true
+            }
+        }
+    }
+
+    return false
+};
+
+    // Отрисовка фигуры
+    // set lockPiece(value) {
+    //     const { y: pieceY, x: pieceX, blocks } = this.activePiece
+    //     console.log(blocks)
+    //     this.playfied[0][0] = 54
+    //     for (let y = 0; y < blocks.length; y++) {
+
+    //         for (let x = 0; x < blocks[y].length; x++) {
+
+    //             if (blocks[y][x]) {
+    //                 console.log("Я работаю")
+
+    //                 this.playfied[pieceY + y][pieceX + x] = blocks[y][x];
+    //             }
+    //         }
+    //     }
+    // }
+  
 }
